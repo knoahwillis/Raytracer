04e7b794083e9c09d39c349528e1be82b559549e
Noah <nwillis@oxy.edu>
2022-11-24 14:13:54 -0600

    Added shadows and reflections

--- src/main.cpp
+++ src/main.cpp
@@ -6,37 +6,19 @@
 #include <glm/geometric.hpp>
 #include <iostream>
 #include <memory>
+#include <utility>
 #include <vector>
 
-float f_computeLighting(std::vector<Light> lights, glm::vec3 intersectionPoint, glm::vec3 intersectionNorm, glm::vec3 V, float s) {
-  float i = 0;
-  glm::vec3 L;
-  for (auto& l : lights) {
-    if (l.getLightType() == AMBIENT) {
-      i += l.f_getIntensity();
-    } else {
-      if (l.getLightType() == POINT) {
-        L = l.vec_getVec() - intersectionPoint;
-      } else {
-        L = l.vec_getVec();
-      }
-    }
+struct Scene {
+  std::vector<Sphere> Spheres;
+  std::vector<Light> Lights;
+  void addSphere(Sphere s) { Spheres.push_back(s); }
+  void addLight(Light l) { Lights.push_back(l); }
+};
 
-    if (glm::dot(intersectionNorm, L) > 0) {
-      i += l.f_getIntensity() * (glm::dot(intersectionNorm, L) / (glm::length(intersectionNorm) * glm::length(L)));
-    }
+glm::vec3 reflectRay(glm::vec3 ray, glm::vec3 norm) { return 2 * glm::dot(norm, ray) * norm - ray; }
 
-    if (s != -1) {
-      glm::vec3 R = (2.0f * intersectionNorm * glm::dot(intersectionNorm, L)) - L;
-      if (glm::dot(R, V) > 0) {
-        i += l.f_getIntensity() * pow((glm::dot(R, V) / (glm::length(R) * glm::length(V))), s);
-      }
-    }
-  }
-  return i;
-}
-
-glm::vec2 vec_sphereRayIntersection(glm::vec3 origin, glm::vec3 direction, Sphere& s) {
+glm::vec2 sphereRayIntersection(glm::vec3 origin, glm::vec3 direction, Sphere& s) {
   float r = s.f_getRadius();
   glm::vec3 CO = origin - s.vec_getCenter();
 
@@ -55,11 +37,11 @@ glm::vec2 vec_sphereRayIntersection(glm::vec3 origin, glm::vec3 direction, Spher
   return ret;
 }
 
-glm::ivec3 vec_traceRay(std::vector<Light> lights, std::vector<Sphere> spheres, glm::vec3 origin, glm::vec3 direction, float t_min, float t_max) {
+std::pair<Sphere, float> closestIntersection(Scene s, glm::vec3 origin, glm::vec3 direction, float t_min, float t_max) {
   float closest_t = MAXFLOAT;
   Sphere closest_sphere;
-  for (auto s : spheres) {
-    glm::vec2 vec_t = vec_sphereRayIntersection(origin, direction, s);
+  for (auto s : s.Spheres) {
+    glm::vec2 vec_t = sphereRayIntersection(origin, direction, s);
     if (vec_t.x >= t_min && vec_t.x <= t_max && vec_t.x < closest_t) {
       closest_t = vec_t.x;
       closest_sphere = s;
@@ -69,23 +51,86 @@ glm::ivec3 vec_traceRay(std::vector<Light> lights, std::vector<Sphere> spheres, 
       closest_sphere = s;
     }
   }
-  if (closest_sphere.f_getRadius() < 0) {
-    return glm::vec3(255, 255, 255);
+  return std::make_pair(closest_sphere, closest_t);
+}
+
+float computeLighting(Scene s, glm::vec3 intersectionPoint, glm::vec3 intersectionNorm, glm::vec3 V, float spec) {
+  float i = 0;
+  float t_max;
+  glm::vec3 L;
+  for (auto& l : s.Lights) {
+    if (l.getLightType() == AMBIENT) {
+      i += l.f_getIntensity();
+    } else {
+      if (l.getLightType() == POINT) {
+        L = l.vec_getVec() - intersectionPoint;
+        t_max = 1;
+      } else {
+        L = l.vec_getVec();
+        t_max = MAXFLOAT;
+      }
+    }
+
+    // shadow check
+    std::pair<Sphere, float> p_shadow = closestIntersection(s, intersectionPoint, L, .001, t_max);
+    if (p_shadow.first.f_getRadius() > -1) {
+      return i;
+    }
+
+    // diffuse lighting
+    if (glm::dot(intersectionNorm, L) > 0) {
+      i += l.f_getIntensity() * (glm::dot(intersectionNorm, L) / (glm::length(intersectionNorm) * glm::length(L)));
+    }
+
+    // specular lighting
+    if (spec != -1) {
+      glm::vec3 R = (2.0f * intersectionNorm * glm::dot(intersectionNorm, L)) - L;
+      if (glm::dot(R, V) > 0) {
+        i += l.f_getIntensity() * pow((glm::dot(R, V) / (glm::length(R) * glm::length(V))), spec);
+      }
+    }
+  }
+  return i;
+}
+
+glm::vec3 traceRay(Scene s, glm::vec3 origin, glm::vec3 direction, float t_min, float t_max, int reflectRecurse) {
+  std::pair<Sphere, float> p_closestSphereFloat = closestIntersection(s, origin, direction, t_min, t_max);
+  if (p_closestSphereFloat.first.f_getRadius() == -1) {
+    return glm::vec3(0, 0, 0);
   }
 
-  glm::vec3 point = origin + closest_t * direction;
-  glm::vec3 norm = glm::normalize(point - closest_sphere.vec_getCenter());
-  glm::ivec3 tester = closest_sphere.vec_getColor() * f_computeLighting(lights, point, norm, -direction, closest_sphere.f_getSpecular());
-  if(tester.x > 255) {
-    tester.x = 255;
+  glm::vec3 P = origin + p_closestSphereFloat.second * direction;
+  glm::vec3 N = glm::normalize(P - p_closestSphereFloat.first.vec_getCenter());
+
+  glm::vec3 color = p_closestSphereFloat.first.vec_getColor() * computeLighting(s, P, N, -direction, p_closestSphereFloat.first.f_getSpecular());
+
+  float r = p_closestSphereFloat.first.f_getReflective();
+  if (r <= 0 || reflectRecurse <= 0) {
+    if (color.x > 255) {
+      color.x = 255;
+    }
+    if (color.y > 255) {
+      color.y = 255;
+    }
+    if (color.z > 255) {
+      color.z = 255;
+    }
+    return color;
   }
-  if(tester.y > 255) {
-    tester.y = 255;
+  glm::vec3 R = reflectRay(-direction, N);
+  glm::vec3 reflectedColor = traceRay(s, P, R, 0.001f, MAXFLOAT, reflectRecurse - 1);
+
+  glm::vec3 retColor = color * (1 - r) + reflectedColor * r;
+  if (retColor.x > 255) {
+    retColor.x = 255;
   }
-  if(tester.z > 255) {
-    tester.z = 255;
+  if (retColor.y > 255) {
+    retColor.y = 255;
   }
-  return tester;
+  if (retColor.z > 255) {
+    retColor.z = 255;
+  }
+  return retColor;
 }
 
 int main(int argc, char* argv[]) {
@@ -95,24 +140,23 @@ int main(int argc, char* argv[]) {
   bool closed = false;
 
   Canvas canvas(800, 800);
-  std::vector<Sphere> Spheres;
-  std::vector<Light> Lights;
+  Scene scene;
 
-  Spheres.push_back(Sphere(1, 500, glm::vec3(0.0f, -1.0f, 3.0f), glm::ivec3(255, 0, 0)));
-  Spheres.push_back(Sphere(1, 500, glm::vec3(2.0f, 0.0f, 4.0f), glm::ivec3(0, 0, 255)));
-  Spheres.push_back(Sphere(1, 10, glm::vec3(-2.0f, 0.0f, 4.0f), glm::ivec3(0, 255, 0)));
-  Spheres.push_back(Sphere(5000, 1000, glm::vec3(0, -5001, 0), glm::vec3(255, 255, 0)));
+  scene.addSphere(Sphere(1, 500, 0.2f, glm::vec3(0.0f, -1.0f, 3.0f), glm::ivec3(255, 0, 0)));
+  scene.addSphere(Sphere(1, 500, 0.3f, glm::vec3(2.0f, 0.0f, 4.0f), glm::ivec3(0, 0, 255)));
+  scene.addSphere(Sphere(1, 10, 0.4f, glm::vec3(-2.0f, 0.0f, 4.0f), glm::ivec3(0, 255, 0)));
+  scene.addSphere(Sphere(5000, 1000, 0.5f, glm::vec3(0, -5001, 0), glm::vec3(255, 255, 0)));
 
-  Lights.push_back(Light(AMBIENT, 0.2));
-  Lights.push_back(Light(POINT, 0.6, glm::vec3(2.0f, 1.0f, 0.0f)));
-  Lights.push_back(Light(DIRECTIONAL, 0.2, glm::vec3(1.0f, 4.0f, 4.0f)));
+  scene.addLight((Light(AMBIENT, 0.2)));
+  scene.addLight((Light(POINT, 0.6, glm::vec3(2.0f, 1.0f, 0.0f))));
+  scene.addLight((Light(DIRECTIONAL, 0.2, glm::vec3(1.0f, 4.0f, 4.0f))));
 
   SDL_RenderClear(rend);
   glm::vec3 Origin = {0.0f, 0.0f, 0.0f};
   for (int j = -800 / 2; j < 800 / 2; j++) {
     for (int i = -800 / 2; i < 800 / 2; i++) {
       glm::vec3 Direction = canvas.vec_canvasToViewpoint(glm::ivec2(i, j));
-      glm::ivec3 color = vec_traceRay(Lights, Spheres, Origin, Direction, 1.0f, MAXFLOAT);
+      glm::vec3 color = traceRay(scene, Origin, Direction, 1.0f, MAXFLOAT, 1);
       SDL_SetRenderDrawColor(rend, color.r, color.g, color.b, 0);
       glm::ivec2 screenPoint = canvas.vec_canasToScreen(glm::ivec2(i, j));
       SDL_RenderDrawPoint(rend, screenPoint.x, screenPoint.y);
